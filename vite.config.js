68e8c92b59c345313dde5df24f349a73b3a87162
Daniil <dtuneev@gmail.com>
2022-09-12 15:14:15 +0300

    add port variable in config2

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
+import * as dotenv from 'dotenv'
 
-export default ({ mode }) => {
-  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+dotenv.config()
 
+const port = process.env;
+
+console.log(port.PORT)
+
+export default () => {
   return defineConfig({
-    plugins: [react()],
+    plugins: [
+      react(),
+    ],
     server: {
-      port: process.env.VITE_PORT,
+      port: port.PORT,
       // https: true
     },
   });
