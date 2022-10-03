de9ed155d707dc8695c34c31df993fed954a0519
Daniil <dtuneev@gmail.com>
2022-10-03 13:31:04 +0300

    validation v1

--- src/popups/Registration.jsx
+++ src/popups/Registration.jsx
@@ -7,6 +7,7 @@ import Button from '@mui/material/Button';
 import GoogleIcon from '../components/icons/Google'
 import FacebookIcon from '../components/icons/Facebook'
 import { toast } from 'react-toastify';
+import { ValidationGroup, Validate } from 'mui-validate';
 
 function PopupLogin() {
   const notify = () => {
@@ -17,20 +18,42 @@ function PopupLogin() {
   return (
     <PopupBase title='Registration'>
       <div className='flex gap-5 items-stretch'>
-        <FormControl className='form-one-column'>
-          <TextField required label='email' variant='outlined' size='small'/>
-          <TextField required label='password' variant='outlined' size='small'/>
-          <Button
-            sx={{ borderRadius: 50, height: 49, width: '100%', fontSize: 18, textTransform: 'none' }}
-            variant='contained'
-            color='primary'
-            endIcon={<ArrowForwardIosIcon />}
-            size='large'
-            onClick={notify}
-          >
-            Continue
-          </Button>
-        </FormControl>
+        <ValidationGroup>
+          <FormControl className='form-one-column'>
+            <Validate
+              name="internal key 1"
+              regex={/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/}
+              required
+              initialValidation='silent'
+            >
+              <TextField
+                id='registration-email'
+                type='email'
+                label='email'
+                variant='outlined'
+                size='small'/>
+            </Validate>
+            <Validate name="internal key 2" required>
+              <TextField
+                id='registration-password'
+                label='password'
+                variant='outlined'
+                size='small'
+                type='password'
+              />
+            </Validate>
+            <Button
+              sx={{ borderRadius: 50, height: 49, width: '100%', fontSize: 18, textTransform: 'none' }}
+              variant='contained'
+              color='primary'
+              endIcon={<ArrowForwardIosIcon />}
+              size='large'
+              onClick={notify}
+            >
+              Log in
+            </Button>
+          </FormControl>
+        </ValidationGroup>
         <div className='vertical-divider'></div>
         <div className='flex flex-col gap-5'>
           Log in using:
