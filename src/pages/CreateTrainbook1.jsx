46b54f364e9082435cd0d5036ac6fe2512ad19b5
Daniil <dtuneev@gmail.com>
2022-11-11 22:45:33 +0200

    emoji selector

--- src/pages/CreateTrainbook1.jsx
+++ src/pages/CreateTrainbook1.jsx
@@ -1,17 +1,18 @@
 import MainLayout from '../components/layouts/Main';
 import FloatToggle from '../components/ui/FloatToggle';
-import Trainbook from '../components/blocks/Trainbook';
 import Button from '@mui/material/Button';
 
-import Grid from "@mui/material/Grid";
+import EmojiPicker from 'emoji-picker-react';
 import {Link} from "react-router-dom";
 import FormControl from "@mui/material/FormControl";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import {Validate} from "mui-validate";
+import TextField from "@mui/material/TextField";
 
 function Trainbooks(props) {
   const menuItems = [
-    {to: '/trainbooks', title: 'public'},
-    {to: '/trainbooks/saved', title: 'private'},
+    {title: 'public'},
+    {title: 'private'},
   ]
 
   return (
@@ -20,7 +21,7 @@ function Trainbooks(props) {
       topRight={
         <Button
           endIcon={<ArrowForwardIosIcon />}
-          Continue
+          outlined
           component={Link}
           to="/login"
         >
@@ -28,15 +29,29 @@ function Trainbooks(props) {
         </Button>
       }
     >
-      <div className='trainbooks'>
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <Trainbook />
-          </Grid>
-          <Grid item xs={6}>
-            <Trainbook />
-          </Grid>
-        </Grid>
+      <div className='create-trainbook flex'>
+        <div>
+          <EmojiPicker />
+        </div>
+        <Validate
+          classes={{root: 'w-full'}}
+          name="internal key 1"
+          regex={/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/}
+          initialValidation='silent'
+        >
+          <TextField
+            id='login-email'
+            type='text'
+            label='Name your trainbook'
+            variant='outlined'
+            size='small'
+            autoFocus={true}
+            fullWidth
+          />
+        </Validate>
+
+
+        <FloatToggle items={menuItems}/>
       </div>
 
       {/*<Button*/}
